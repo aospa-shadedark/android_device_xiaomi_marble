fabdc65ea69177819a1d37c98f835f7ac72dd6c2
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-03-27 23:00:07 +0530

    dolby: Add SpatialAudio support
    Change-Id: Ia565fa0e3f07acd02d33492a72bf9921e9542847

--- device.mk
+++ device.mk
@@ -28,7 +28,8 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Dolby
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
+    $(LOCAL_PATH)/configs/dolby/dax-default-spatializer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dax-default-spatializer.xml
 
 # Fingerprint
 TARGET_USES_MFP_DAEMON := true
