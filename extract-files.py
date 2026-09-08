2bd60d7107ad71321c26644f8fecbf0d647747d7
ShadeDark6 <nizamfa6@gmail.com>
2026-09-08 15:43:01 +0000

    marble: Drop duplicate displayfeature prebuilt
    Drop the device-specific vendor.xiaomi.hardware.displayfeature@1.0 prebuilt and use the hardware/xiaomi one instead.
    
    error: ninja: out/soong/build.aospa_marble.0.ninja:538114: multiple rules generate out/target/product/marble/symbols/vendor/lib64/vendor.xiaomi.hardware.displayfeature@1.0.so [-w dupbuild=err]
    
    Signed-off-by: ShadeDark6 <nizamfa6@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -61,7 +61,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.ims.*',
         'vendor.qti.latency*',
         'vendor.xiaomi.hardware.campostproc@1.0',
-        'vendor.xiaomi.hardware.displayfeature@1.0',
     ): lib_fixup_vendor_suffix,
     'libwpa_client': lib_fixup_remove,
 }
